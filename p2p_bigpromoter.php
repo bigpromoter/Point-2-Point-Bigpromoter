e0e0681dbf4328c2ace47714ef7d7d8c045799bb
Mauro Baptista <mcnardelli@gmail.com>
2015-02-04 21:57:16 -0800

    Change Version

--- p2p_bigpromoter.php
+++ p2p_bigpromoter.php
@@ -1,15 +1,15 @@
 <?php //Info
 /**
  * @package P2P BigPromoter
- * @version 1.0
+ * @version Alpha 1.0
  */
 /*
 Plugin Name: P2P BigPromoter
-Plugin URI: http://wordpress.org/plugins/P2PBigPromoter/
-Description: xxx
-Author: xxx
-Version: 1.0
-Author URI: http://xx.xx/
+Plugin URI: http://wordpress.org/plugins/P2P_Big_Promoter/
+Description: Plugin to calculate fare for transportation company.
+Author: Big Promoter
+Version: Alpha 1.0
+Author URI: http://bigpromoter.com/
 License: GPL2
 */
 if ( !function_exists( 'add_action' ) ) {
